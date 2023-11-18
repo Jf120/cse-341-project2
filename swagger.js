efe3a75b8fad1824d3265b717a2eb69c4450828c
Jf120 <jafc120@gmail.com>
2023-11-17 21:36:43 -0600

    host updated

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: "Project 2 API Information",
         version: "1.0.0"
     },
-    host: "localhost:3030",
+    host: "https://project2-am6a.onrender.com",
     schemes: ['http', 'https']
 };
 
