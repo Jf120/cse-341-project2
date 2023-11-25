41b39924a068dc19bfcbeaa645a9596164e793d9
Jf120 <jafc120@gmail.com>
2023-11-24 22:50:49 -0600

    added authentication

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
+router.use('/users', require('./users'));
+router.use('/info', require('./info'));
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
 });
 
-router.use('/users', require('./users'));
-
-router.use('/info', require('./info'));
 
 module.exports = router;    
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
+const { isAuthenticated } = require('../middleware/authenticate');
 const validation = require('../middleware/validate');
 
 router.get('/', usersController.getAll);
 
 router.get('/:id', usersController.getSingle);
 
-router.post('/', validation.saveUser, usersController.createUser);
+router.post('/', isAuthenticated, validation.saveUser, usersController.createUser);
 
-router.put('/:id', validation.saveUser, usersController.updateUser);
+router.put('/:id', isAuthenticated, validation.saveUser, usersController.updateUser);
 
-router.delete('/:id', usersController.deleteUser);
+router.delete('/:id', isAuthenticated, usersController.deleteUser);
 
 module.exports = router;
